d9f8e37a67f759601673f2b51fc4986fb0beb642
JJhwan7099 <ghks7099@naver.com>
2025-10-25 15:38:34 +0900

    feat: 입력값을 담아서 파싱 후 감싸기 위한 dto 구현

---
+++ src/main/java/dto/GameInitDto.java
@@ -0,0 +1,21 @@
+package dto;
+
+import java.util.List;
+
+public class GameInitDto {
+    private final List<String> carNames;
+    private final int gameCount;
+
+    public GameInitDto(List<String> carNames, int gameCount) {
+        this.carNames = carNames;
+        this.gameCount = gameCount;
+    }
+
+    public List<String> getCarNames() {
+        return carNames;
+    }
+
+    public int getGameCount() {
+        return gameCount;
+    }
+}
