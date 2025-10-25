e70ca0dc4acd92244bb07b9966fd349706e64b3e
JJhwan7099 <ghks7099@naver.com>
2025-10-25 15:39:14 +0900

    feat(Inputparser.java): InputParser 구현

---
+++ src/main/java/model/InputParser.java
@@ -0,0 +1,26 @@
+package model;
+
+import dto.GameInitDto;
+import validator.InputParserValidator;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputParser {
+    public GameInitDto parseInput(String carNames, String gameCount) {
+        List<String> cars = Arrays.stream(carNames.split(",", -1))
+                .map(String::trim)
+                .toList();
+        InputParserValidator.validateCarNames(cars);
+
+        int parsedGameCount;
+        try{
+            parsedGameCount = Integer.parseInt(gameCount);
+            InputParserValidator.validateGameCount(parsedGameCount);
+        } catch(NumberFormatException e) {
+            throw new IllegalArgumentException("게임 횟수는 1이상의 정수입니다.");
+        }
+
+        return new GameInitDto(cars, parsedGameCount);
+    }
+}
