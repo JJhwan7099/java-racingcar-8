2462ab496775b9ae5a2699c59800d5f40cfe3e59
JJhwan7099 <ghks7099@naver.com>
2025-10-26 22:18:38 +0900

    feat(InputParserValidator.java): 참가하는 자동차 수가 2대이상인지 확인하는 로직 구현

--- src/main/java/validator/InputParserValidator.java
+++ src/main/java/validator/InputParserValidator.java
@@ -7,6 +7,10 @@ public class InputParserValidator {
     private static final String ALPHABET_PATTERN = "[a-zA-Z]+$";
 
     public static void validateCarNames(List<String> cars) {
+        if(cars.size() < 2) {
+            throw new IllegalArgumentException("참가하는 자동차는 2대 이상이어야 합니다.");
+        }
+
         for(String carName : cars) {
             if(!carName.matches(ALPHABET_PATTERN) || carName.isBlank() || carName.length() > 5)
                 throw new IllegalArgumentException("자동차명은 빈값 혹은 공백이 될 수 없으며, 5글자 이하의 알파벳으로만 구성되어야 합니다.");
