9cca344f3f09fa2796b0ac8ba51ecbb8038b42cf
JJhwan7099 <ghks7099@naver.com>
2025-10-27 18:07:24 +0900

    test(CarListTest): 자동차 경주 과제 제한사항에 맞게 자동차명에 알파벳만 있도록 수정

--- src/test/java/racingcar/model/car/CarListTest.java
+++ src/test/java/racingcar/model/car/CarListTest.java
@@ -17,7 +17,7 @@ public class CarListTest {
 
     @Test
     void 자동차를_등록하면_cars에_추가된다() {
-        Car car1 = new Car("car1");
+        Car car1 = new Car("car");
         carList.registerCar(car1);
 
         assertThat(carList.getCars()).hasSize(1);
@@ -26,35 +26,35 @@ public class CarListTest {
 
     @Test
     void getCars를_호출하면_등록된_자동차들이_반환된다() {
-        Car car1 = new Car("car1");
-        Car car2 = new Car("car2");
-        Car car3 = new Car("car3");
+        Car carA = new Car("carA");
+        Car carB = new Car("carB");
+        Car carC = new Car("carC");
 
-        carList.registerCar(car1);
-        carList.registerCar(car2);
-        carList.registerCar(car3);
+        carList.registerCar(carA);
+        carList.registerCar(carB);
+        carList.registerCar(carC);
 
         assertThat(carList.getCars()).hasSize(3);
-        assertThat(carList.getCars()).contains(car1, car2, car3);
+        assertThat(carList.getCars()).contains(carA, carB, carC);
     }
 
     @Test
     void findCarsWithMaxPosition을_호출하면_position이_최대인_자동차_목록을_반환한다() {
-        Car car1 = new Car("car1");
-        Car car2 = new Car("car2");
-        Car car3 = new Car("car3");
+        Car carA = new Car("carA");
+        Car carB = new Car("carB");
+        Car carC = new Car("carC");
 
-        carList.registerCar(car1);
-        carList.registerCar(car2);
-        carList.registerCar(car3);
+        carList.registerCar(carA);
+        carList.registerCar(carB);
+        carList.registerCar(carC);
 
-        car1.moveForward();
-        car1.moveForward();
-        car2.moveForward();
-        car3.moveForward();
-        car3.moveForward();
+        carA.moveForward();
+        carA.moveForward();
+        carB.moveForward();
+        carC.moveForward();
+        carC.moveForward();
 
         assertThat(carList.findCarsWithMaxPosition()).hasSize(2);
-        assertThat(carList.findCarsWithMaxPosition()).contains(car1, car3);
+        assertThat(carList.findCarsWithMaxPosition()).contains(carA, carC);
     }
 }
